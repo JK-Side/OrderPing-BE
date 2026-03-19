2327d3c7ad7d86ed96d112af5e73cfb7b9f63544
dradnats1012 <herjebi1012@gmail.com>
2026-03-19 21:12:25 +0900

    fix: 글자 수 제한 추가

--- orderping-api/src/main/java/com/orderping/api/store/dto/StoreCreateRequest.java
+++ orderping-api/src/main/java/com/orderping/api/store/dto/StoreCreateRequest.java
@@ -3,12 +3,15 @@ package com.orderping.api.store.dto;
 import jakarta.validation.constraints.Size;
 
 public record StoreCreateRequest(
-    @Size(max = 50, message = "주점명은 50자를 초과할 수 없습니다.")
+    @Size(max = 10, message = "주점명은 10자를 초과할 수 없습니다.")
     String name,
+    @Size(max = 100, message = "주점 설명은 100자를 초과할 수 없습니다.")
     String description,
     String imageUrl,
     String bankCode,
+    @Size(max = 6, message = "예금주명은 6자를 초과할 수 없습니다.")
     String accountHolder,
+    @Size(max = 20, message = "계좌번호는 20자를 초과할 수 없습니다.")
     String accountNumber
 ) {
 }
